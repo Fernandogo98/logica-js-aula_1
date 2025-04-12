66405ba5c216f0d96be42afbd33f96ce33b42230
Fernandogo98 <nandojunior34@gmail.com>
2025-04-12 10:23:39 -0300

    V3-Console.log Test

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 1;
-console.log(numeroSecreto);
+let numeroSecreto = 7;
+console.log(`Valor do Numero Secreto: ${numeroSecreto}`);
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log(`Valor do chute: ${chute}`);
 
 //se o chute for = ao numero secreto
 if (chute == numeroSecreto) {

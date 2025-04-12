4111574b12c1d597c780e6fcf8665db57921cacc
Fernandogo98 <nandojunior34@gmail.com>
2025-04-12 12:45:52 -0300

    V3

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 7;
 console.log(`Valor do Numero Secreto: ${numeroSecreto}`);
 let chute;
-console.log(`Valor do chute: ${chute}`); //mostra o valor do chute inserido no prompt
+let tentativas = 1;
 
 //Enquanto = repetição, não for igual ao numero secreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
+    console.log(`Valor do chute: ${chute}`); //mostra o valor do chute inserido no prompt
     //se o chute for = ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com: ${tentativas} Tentativas`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O numero secreto é menor que: ${chute}`);
         } else {
             alert(`O numero secreto é maior que: ${chute}`);
         }
+        //tentativas = tentativas + 1;
+        tentativas++;
 
     }
 }

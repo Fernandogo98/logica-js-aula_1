a4ebc72dfba67dd5ddd98c6381729e1cf926873d
Fernandogo98 <nandojunior34@gmail.com>
2025-04-12 10:05:08 -0300

    V3-Att

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 8;
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 1;
 console.log(numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 10');
 
 //se o chute for = ao numero secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai! Você descobriu o número secreto (8)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 }else{
     alert("Você errou! :(")
 }
